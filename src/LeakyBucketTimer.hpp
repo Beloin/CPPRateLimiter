43a9cd9534da2e4e980becda5b4775cb0fef8cf3
Beloin Sena <beloin.rodrigues@gmail.com>
2025-01-20 21:59:11 -0300

    WIP - Maybe use ticks?

--- src/LeakyBucketTimer.hpp
+++ src/LeakyBucketTimer.hpp
@@ -6,11 +6,12 @@
 #define SRC_LEAKYBUCKETTIMER_H
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "LeakyBucketQueue.hpp"
 #include "RateLimiter.hpp"
 #include <boost/asio/deadline_timer.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/system/detail/error_code.hpp>
-class LeakyBucketTimer {
+class LeakyBucketTimer : public LeakyBucketQueue{
 
 public:
   LeakyBucketTimer() {
@@ -21,12 +22,15 @@ public:
   // TODO: Maybe focus only in the thing to do, not in the timer
   // So we can create the timer in the main class
   void run();
-  void addConnection(SharedConnection);
   // TODO: Or implement the "RateLimiter" here and make the timer in run, 
   // Implement the RateLimiter and add the timer, so everytime we add a thing here 
   // we can wait only for the timer. Actually the LeakyBucketQueue can be the DefaultRateLimiter, but with a timer
   // Or we can remplement the RateLimiter here and add the timer as background job, respecting an setup
   // function 
+  // Or just a tick function that uses the dictated time
+  // Should be ticked in $setup time
+  // Create a tick time configuration? So the timer is inside the tick?
+  // void tick();
 
 private:
 };
