acd6a48ab0a6409572308ede747da1f127d56d48
Beloin Sena <beloin.rodrigues@gmail.com>
2025-01-20 21:56:37 -0300

    WIP - How to call timer

--- src/LeakyBucketTimer.cpp
+++ src/LeakyBucketTimer.cpp
@@ -1,8 +1,25 @@
 #include "LeakyBucketTimer.hpp"
 #include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
 #include <boost/make_shared.hpp>
+#include <functional>
 
 using namespace boost;
 using Timer = shared_ptr<asio::deadline_timer>;
 
-void LeakyBucketTimer::run(int milli) {}
+void LeakyBucketTimer::setup(int milli, boost::asio::io_context ctx) {
+  boost::posix_time::millisec ms{milli};
+  timer = boost::asio::deadline_timer{ctx, ms};
+
+  // Reschedule the timer for 1 second in the future:
+  timer.expires_at(timer.expires_at() + ms);
+  // Posts the timer event
+  timer.async_wait(bind(tick, asio::placeholders::error));
+}
+
+void LeakyBucketTimer::tick(const boost::system::error_code &) {
+  // Reschedule the timer for 1 second in the future:
+  timer.expires_at(timer.expires_at() + ms);
+  // Posts the timer event
+  timer.async_wait(tick);
+}
--- src/LeakyBucketTimer.hpp
+++ src/LeakyBucketTimer.hpp
@@ -6,16 +6,27 @@
 #define SRC_LEAKYBUCKETTIMER_H
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "RateLimiter.hpp"
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/system/detail/error_code.hpp>
 class LeakyBucketTimer {
 
 public:
-  LeakyBucketTimer() = default;
+  LeakyBucketTimer() {
+
+  };
   ~LeakyBucketTimer() = default;
 
-  /**
-   * @brief Asyncronous run
-   */
-  void run(int milli);
+  // TODO: Maybe focus only in the thing to do, not in the timer
+  // So we can create the timer in the main class
+  void run();
+  void addConnection(SharedConnection);
+  // TODO: Or implement the "RateLimiter" here and make the timer in run, 
+  // Implement the RateLimiter and add the timer, so everytime we add a thing here 
+  // we can wait only for the timer. Actually the LeakyBucketQueue can be the DefaultRateLimiter, but with a timer
+  // Or we can remplement the RateLimiter here and add the timer as background job, respecting an setup
+  // function 
 
 private:
 };
